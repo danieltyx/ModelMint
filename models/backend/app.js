cdbb96fa9f1c4f2967acb1a9dae0fe4cc1104f6c
Yuxuan Tian <danieltian.yx@gmail.com>
2023-05-19 14:30:04 -0700

    write to single doc

--- models/backend/app.js
+++ models/backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 4001;
 const db = require('./firebaseConfig');
+const { getFirestore, collection, doc, setDoc, addDoc } = require('firebase/firestore/lite');
 app.use(cors());
 app.use(express.json());
 
@@ -11,8 +12,9 @@ app.use(express.json());
 app.post('/create', async (req, res) => {
     
     const data = req.body;
-    // const Users = db.collection('Users');
-    // await Users.add(data);
+    const userId = 's0x425d06f5218f19b51fb3415fbaaa00cccab61cef'; // Replace with the ID of the specific document you want to write to
+    const userDocRef = doc(db, 'Users', userId);
+    await setDoc(userDocRef, data);
     console.log("Data of the request: ", data);
     res.send({ message: 'We received your data!!!' });
 

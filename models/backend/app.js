79f9d4beb3329144d05dfb041c9c955eadc5e270
Yuxuan Tian <danieltian.yx@gmail.com>
2023-05-19 14:31:36 -0700

    and read

--- models/backend/app.js
+++ models/backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 4001;
 const db = require('./firebaseConfig');
-const { getFirestore, collection, doc, setDoc, addDoc } = require('firebase/firestore/lite');
+const { getFirestore, collection, doc, setDoc,getDoc, addDoc } = require('firebase/firestore/lite');
 app.use(cors());
 app.use(express.json());
 
@@ -18,6 +18,21 @@ app.post('/create', async (req, res) => {
     console.log("Data of the request: ", data);
     res.send({ message: 'We received your data!!!' });
 
+});
+
+
+app.get('/read', async (req, res) => {
+    const userId = 's0x425d06f5218f19b51fb3415fbaaa00cccab61cef'; // Replace with the ID of the specific document you want to write to
+    const userDocRef = doc(db, 'Users', userId);
+    const docSnap = await getDoc(userDocRef);
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        res.send(docSnap.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        res.send({ message: 'No such document!' });
+    }
 });
 
 app.listen(port, () => {

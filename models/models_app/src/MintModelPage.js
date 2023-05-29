f229581f9cc64da29645eeba2f3d469c859d7347
dapanji.eth <tingyan.deng@vanderbilt.edu>
2023-05-29 12:10:57 -0700

    mintmodel

--- models/models_app/src/MintModelPage.js
+++ models/models_app/src/MintModelPage.js
@@ -1,18 +1,64 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import type { FC } from 'react';
-import React from 'react';
 import classes from './App.module.css';
 import resets from './components/Mint/_resets.module.css';
 import { MintModel } from './components/Mint/MintModel/MintModel';
+import { ethers } from 'ethers';
 
 interface Props {
   className?: string;
 }
+
 export const MintModelPage: FC<Props> = memo(function App(props = {}) {
-  function handleMint() {
-    console.log("mint clikced");
-    //Nick: start your code here 
+  // Custom OKExChain mainnet provider
+  const customOKExChainProvider = new ethers.providers.JsonRpcProvider({
+    url: "https://exchainrpc.okex.org", // OKExChain mainnet node URL
+    chainId: 66, // OKExChain mainnet chainId
+    name: "okexchain"
+  });
+
+  // Set provider and signer
+  const [provider, setProvider] = useState(customOKExChainProvider);
+  const [signer, setSigner] = useState(provider.getSigner());
+  
+  async function handleMint() {
+    console.log("mint clicked");
+
+    // The contract address of the NFT
+    const contractAddress = "0xda0d7f342b9c0f7f5f456e0c0a3ec6fe925eaef3";
+
+    // The Token ID of the NFT you want to send
+    const tokenId = "9568";
+
+    // The address of the user's wallet
+    const userAddress = "0x6F83E25c9f4348Eefb0766319093d82EA9777300";
+
+    // The ABI of the contract (You can get this from the contract's documentation or Etherscan)
+    const contractABI = []; // replace this with your contract's ABI
+
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    // Estimate the gas price
+    const estimate = await contract.estimateGas.transferFrom(
+      await signer.getAddress(), // from your wallet
+      userAddress, // to user's wallet
+      tokenId // the token id you want to transfer
+    );
+
+    // Execute the transaction
+    const transaction = await contract.transferFrom(
+      await signer.getAddress(),
+      userAddress,
+      tokenId,
+      { gasLimit: estimate }
+    );
+
+    // Wait for it to be mined
+    await transaction.wait();
+
+    console.log(`Sent token #${tokenId} to ${userAddress}!`);
   }
+
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
       <MintModel onMint={handleMint}/>

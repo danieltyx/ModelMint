cd136ab7463fcf0b56404fe2818d41c00a0ddcee
dapanji.eth <tingyan.deng@vanderbilt.edu>
2023-05-29 22:30:56 -0700

    Update zoksh.js

--- models/models_app/src/zoksh.js
+++ models/models_app/src/zoksh.js
@@ -4,11 +4,12 @@ function zokshUrl(){
     // REQUIRED api key available in your merchant dashboard
     const apiKey = '647504c2d2be1cb9419b6f4f';
     
-    const cryptos = ['ETH','BTC','OKX'];
-    const networks = ['ethereum', 'bitcoin','okexchain'];
+    const cryptos = ['ETH','BTC','OKT'];
+    const networks = ['ethereum', 'bitcoin','OKExChain'];
     const wallets = {
         ethereum: '0x425d06f5218f19b51fB3415FbAAa00CCCAB61cEf',
-        bitcoin: 'bc1qxy2kgdygjrsqtzq2n0yrf****************'
+        bitcoin: 'bc1qxy2kgdygjrsqtzq2n0yrf****************',
+        okexchain: '0xb69d6140df9b83c4fcee30c7f720a2993aced279'
     }
     const name = 'Demo user';
     const email = 'demo@demo.com';

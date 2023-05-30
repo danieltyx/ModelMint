8892dbbbfe214928d55d276072bac23a41fc74ba
Yuxuan Tian <danieltian.yx@gmail.com>
2023-05-29 17:32:55 -0700

    Update CreateButton_statusDefaultBoug.tsx

--- models/models_app/src/components/mainpage/HomepageLoggedIn/CreateButton_statusDefaultBoug/CreateButton_statusDefaultBoug.tsx
+++ models/models_app/src/components/mainpage/HomepageLoggedIn/CreateButton_statusDefaultBoug/CreateButton_statusDefaultBoug.tsx
@@ -5,8 +5,7 @@ import resets from '../../_resets.module.css';
 import { CreateButton_statusDefaultSize2 } from '../CreateButton_statusDefaultSize2/CreateButton_statusDefaultSize2';
 import { IconCollection_iconBuyStatusDe } from '../IconCollection_iconBuyStatusDe/IconCollection_iconBuyStatusDe';
 import classes from './CreateButton_statusDefaultBoug.module.css';
-import { Local_mallIcon } from './Local_mallIcon';
-
+import LocalMallIcon from '@mui/icons-material/LocalMall';
 interface Props {
   className?: string;
   classes?: {
@@ -16,8 +15,12 @@ interface Props {
 }
 /* @figmaId 456:5698 */
 export const CreateButton_statusDefaultBoug: FC<Props> = memo(function CreateButton_statusDefaultBoug(props = {}) {
+  function handleBuy(){
+    console.log("Buy button clicked");
+    
+  }
   return (
-    <button className={`${resets.clapyResets} ${props.classes?.root || ''} ${props.className || ''} ${classes.root}`}>
+    <button onClick={handleBuy} className={`${resets.clapyResets} ${props.classes?.root || ''} ${props.className || ''} ${classes.root}`}>
       <CreateButton_statusDefaultSize2
         className={classes.createButton}
         swap={{
@@ -27,8 +30,8 @@ export const CreateButton_statusDefaultBoug: FC<Props> = memo(function CreateBut
               classes={{ local_mall: classes.local_mall }}
               swap={{
                 local_mall: (
-                  <div className={classes.local_mall}>
-                    <Local_mallIcon className={classes.icon} />
+                  <div className={classes.local_mall} style={{'color': 'rgb(22,37,43)'}} >
+                  <LocalMallIcon/>
                   </div>
                 ),
               }}
